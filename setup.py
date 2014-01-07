02ff1feb870c4d8ad7947246ae42bab8dc5c677b
Ben Privat <bprivat1@gmail.com>
2014-01-07 13:50:27 -0600

    Add necessary files to create a distribution

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from distutils.core import setup
 
 setup(name='video_picker',
       version='1.0',
-      description="Randomly plays a video file in a given directory. Recursive selection available.",
+      description="Randomly plays a video file in a given directory or recursively.",
       author='Ben Privat',
       author_email='ben.privat1@gmail.com',
       url='https://github.com/bprivat/video_picker',
+      license='http://opensource.org/licenses/MIT',
+      platforms=['nt', 'posix'],
       py_modules=['video_picker'],
       )
